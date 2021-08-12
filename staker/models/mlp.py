2495014d24fbd7017bb09a4b54922577b52735b1
NicoCoallier <nicolas.coallier@gmail.com>
2021-08-11 20:08:37 -0400

    fix: Dict

--- staker/models/mlp.py
+++ staker/models/mlp.py
@@ -1,19 +1,19 @@
+from typing import Tuple
 
 import tensorflow as tf
-from staker.models.base import TFInputBuilder
-from typing import List, Text, Tuple
 
+from staker.models.base import TFInputBuilder
 
 
 class MLP(TFInputBuilder):
-    """MLP implementation for numerAI competition
-    """
+    """MLP implementation for numerAI competition"""
+
     def __init__(
         self,
         config,
         vocab,
     ):
-        super(TabNet, self).__init__()
+        super(MLP, self).__init__()
         self.config = config
         self.vocab = vocab
 
@@ -23,19 +23,20 @@ class MLP(TFInputBuilder):
         # BatchNorm
         self.bn = tf.keras.layers.BatchNormalization()
         # Hidden
-        self.dense1 = tf.keras.layers.Dense(1024,activation="relu")
+        self.dense1 = tf.keras.layers.Dense(1024, activation="relu")
         self.dropout1 = tf.keras.layers.Dropout(0.1)
-        self.dense2 = tf.keras.layers.Dense(1024,activation="relu")
+        self.dense2 = tf.keras.layers.Dense(1024, activation="relu")
         self.dropout2 = tf.keras.layers.Dropout(0.1)
-        self.dense3 = tf.keras.layers.Dense(1024,activation="relu")
+        self.dense3 = tf.keras.layers.Dense(1024, activation="relu")
         self.dropout3 = tf.keras.layers.Dropout(0.1)
-        self.dense4 = tf.keras.layers.Dense(1024,activation="relu")
+        self.dense4 = tf.keras.layers.Dense(1024, activation="relu")
         self.dropout4 = tf.keras.layers.Dropout(0.1)
         # Output
         self.output_layer = tf.keras.layers.Dense(1, activation="sigmoid")
 
     def call(
-        self, features: tf.Tensor, training: bool = None) -> Tuple[tf.Tensor, tf.Tensor]:
+        self, features: tf.Tensor, training: bool = None
+    ) -> Tuple[tf.Tensor, tf.Tensor]:
         # TO DO: Improve forward pass code
         num, categ = self.encode_inputs(features)
         features = tf.keras.layers.concatenate(num + categ)
